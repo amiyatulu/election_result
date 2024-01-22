c445a808c1bf178b61a91a2f7614d40320558c69
Amiya Behera <amiyatulu@gmail.com>
2024-01-22 11:35:29 +0530

    update

---
+++ 2023_legislative_assembly_election/data_analysis/src/data_analysis/seats_percentage.py
@@ -0,0 +1,34 @@
+# Given seat counts
+# seat_counts = {
+#     "BJP": 163,
+#     "INC": 66,
+#     "BHRTADVSIP": 1
+# }
+
+# seat_counts = {
+#     "BJP": 115,
+#     "INC": 69,
+#     "BHRTADVSIP": 3,
+#     "BSP":2,
+#     "RLD": 1,
+#     "IND": 8,
+# }
+
+
+seat_counts = {
+    "BHRS": 39,
+    "INC": 64,
+    "BJP": 8,
+    "AIMIM": 7,
+    "CPI":1
+}
+
+# Total number of seats
+total_seats = sum(seat_counts.values())
+
+# Calculate percentage for each party
+percentage_seats = {party: (seats / total_seats) * 100 for party, seats in seat_counts.items()}
+
+print("Percentage of Seats:")
+for party, percentage in percentage_seats.items():
+    print(f"{party}: {percentage:.2f}%")
