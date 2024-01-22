6bee5a25ce56ca8c7145611d8e79c908dc80eec1
Amiya Behera <amiyatulu@gmail.com>
2024-01-22 11:09:15 +0530

    percentage votes

---
+++ 2023_legislative_assembly_election/data_analysis/src/data_analysis/vote_percentage.py
@@ -0,0 +1,20 @@
+import json
+
+# Read data from JSON file
+with open('analysis_data/total_votes_by_party_data/total_votes_by_party_data_mizoram.json', 'r') as file:
+    data = json.load(file)
+
+# Calculate total votes
+total_votes = sum(data['VotesByParty'].values())
+
+# Calculate percentage votes for each party
+percentage_votes_by_party = {party: (votes / total_votes) * 100 for party, votes in data['VotesByParty'].items()}
+
+# Create a new dictionary with the results
+result_data = {'PercentageVotesByParty': percentage_votes_by_party}
+
+# Write results to a new JSON file
+with open('analysis_data/total_percentage_by_party_data/percentage_mizoram.json', 'w') as file:
+    json.dump(result_data, file, indent=2)
+
+print("Percentage votes by party calculated and saved to 'percentage_votes_data.json'.")
